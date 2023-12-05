7d85bf9d5a94db43f70b59c968331f510751fbee
Omar Saleem <ofsaleem@users.noreply.github.com>
2023-12-05 13:30:44 -0500

    skeleton

--- 2023/day1.go
+++ 2023/day1.go
@@ -1,3 +1,31 @@
 package main
 
+import (
+  "os"
+  "bufio"
+)
 
+func main() {
+  input, err := os.Open("testinput.txt")
+  if err != nil {
+    panic(err)
+  }
+
+  scanner := bufio.NewScanner(input)
+  scanner.Split(bufio.ScanLines)
+  var lines []string
+  for scanner.Scan() {
+    lines = append(lines, scanner.Text())
+  }
+  input.Close()
+
+
+}
+
+func parseFile(filename string) []string {
+
+}
+
+func parseLine(line string) int {
+
+}
