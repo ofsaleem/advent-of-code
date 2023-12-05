95dbf09894e1d82e86d821531c4c68380d3d4a62
Omar Saleem <ofsaleem@users.noreply.github.com>
2023-12-05 14:45:21 -0500

    converting stuff for part 2

--- 2023/day01/day01.go
+++ 2023/day01/day01.go
@@ -38,8 +38,25 @@ func parseLine(line string) int {
       current = append(current, int(c - '0'))
     }
   }
-  if len(current) >= 1 {
-    val := 10 * current[0] + current[len(current) - 1]
+  
+  var numMap = make(map[string]int)
+  var min [2]int {0, "zero"}
+  var max [2]int {65535, "zero"}
+  numbers := []string {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+  "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+  for _, num := range numbers {
+    if strings.Contains(line, num) {
+      if min[0] >= strings.Index(line, num) {
+        min[0] = strings.Index(line, num)
+        min[1] = unicode.Number
+      }
+    }
+  }
+}
+
+func calcValue(inputs []int) int {
+  if len(inputs) >= 1 {
+    val := 10 * inputs[0] + inputs[len(inputs) - 1]
     return val
   } else {
     return 0
